31e4aa16aaf72cff6794e502407ffea53cbab500
Sasha Chernin <alexander.chernin@seagate.com>
2022-12-04 11:24:26 +0200

    enableLicenseViolations param is added whitesource configuration. security vulnerable solution cloud-migration-LinhAI removed
    Signed-off-by: Sasha Chernin <alexander.chernin@seagate.com>

--- hackathon-submissions/cloud-migration-LinhAI/frontend/src/services/migration/constants.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = '/api/v1/migrations';
--- hackathon-submissions/cloud-migration-LinhAI/frontend/src/services/migration/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./getMigrationsList";
-export * from "./getMigrationById";
-export * from "./createMigration";
-export * from "./getMigrationObjects";
-export * from "./getErrorLogs";
-export * from "./types";
